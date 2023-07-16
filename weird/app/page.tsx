2f2d1b68811cc438ec4eed1d0300e8161179b884
21givenchy <65071041+21givenchy@users.noreply.github.com>
2023-07-16 15:56:22 +0000

    layout start

--- weird/app/page.tsx
+++ weird/app/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1>Hello from mars
-
-   </h1>
+   <div className='bg-white py-6 sn:py-8 lg:py-12 '>
+    <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+      <div className='mb-10 md:mb-16'>
+        <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>
+             Top Treending Movies
+        </h2>
+      </div>
+    </div>
+   </div>
   )
 }
