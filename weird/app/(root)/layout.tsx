0bd6f1e8edf77b65ecf89877138d12e7538a9bb1
palontologist <100496915+palontologist@users.noreply.github.com>
2023-08-11 17:05:51 +0000

    nav

--- weird/app/(root)/layout.tsx
+++ weird/app/(root)/layout.tsx
@@ -8,6 +8,8 @@ const RootLayout = ({
     return (
      <div className="h-full">
         <Navbar />
+        <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+          </div> 
       <main className="md:pl-20 pt-16 h-full">
         {children}
       </main>
