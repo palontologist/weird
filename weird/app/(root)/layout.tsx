0d54ee3c9e3cfdd47f22076b530c6c0902aae7a5
21givenchy <you@example.com>
2023-08-29 13:24:23 +0300

    lets goo

--- weird/app/(root)/layout.tsx
+++ weird/app/(root)/layout.tsx
@@ -11,6 +11,7 @@ const RootLayout = ({
      <div className="h-full">
         <Navbar />
         <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        
           <Sidebar />
         </div> 
       <main className="md:pl-20 pt-16 h-full">
