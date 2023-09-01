e4380febe420d5382bfbfc348ebd79e011bfd198
21givenchy <you@example.com>
2023-09-01 11:23:12 +0300

    prisma db

--- weird/app/layout.tsx
+++ weird/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 
@@ -9,7 +9,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'frontforumfocus',
